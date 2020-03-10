2d4ed52c2f264dbdac8f7b5a1d87779e803b9f8e
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-10 19:14:03 -0400

    New save

--- db/migrate/20200306003909_create_answers.rb
+++ db/migrate/20200306003909_create_answers.rb
@@ -2,7 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.references :question, foreign_key: true
-      t.referencesanswer :user
+      t.references :user, foreign_key: true
+      t.string :answer
 
       t.timestamps
     end
--- db/migrate/20200306004738_create_stories.rb
+++ db/migrate/20200306004738_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration[5.2]
   def change
     create_table :stories do |t|
       t.string :description
-      t.references :author, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :attachment, foreign_key: true
 
       t.timestamps
