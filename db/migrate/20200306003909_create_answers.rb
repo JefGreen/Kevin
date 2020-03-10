70ebfff021e9bb651ba033d6a3da61a6b7cb0364
Lotfi Kaced <lotfikaced@gmail.com>
2020-03-10 19:13:28 -0400

    app is working

--- db/migrate/20200306003909_create_answers.rb
+++ db/migrate/20200306003909_create_answers.rb
@@ -2,9 +2,10 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.references :question, foreign_key: true
-      t.referencesanswer :user
+      t.references :user, foreign_key: true
+      t.string :answer
 
-      t.timestamps
+      t.timestamps 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,95 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_015848) do
+ActiveRecord::Schema.define(version: 2020_03_06_004828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id"
+    t.bigint "user_id"
+    t.string "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "document"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "meeting_location"
+    t.date "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_contacts", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_group_contacts_on_contact_id"
+    t.index ["group_id"], name: "index_group_contacts_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean "liked"
+    t.bigint "tag_id"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_likes_on_contact_id"
+    t.index ["tag_id"], name: "index_likes_on_tag_id"
+  end
+
+  create_table "memories", force: :cascade do |t|
+    t.bigint "story_id"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_memories_on_contact_id"
+    t.index ["story_id"], name: "index_memories_on_story_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.string "correct_answer"
+    t.bigint "contact_id"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_questions_on_contact_id"
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "description"
+    t.bigint "user_id"
+    t.bigint "attachment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attachment_id"], name: "index_stories_on_attachment_id"
+    t.index ["user_id"], name: "index_stories_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +112,15 @@ ActiveRecord::Schema.define(version: 2020_02_28_015848) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "group_contacts", "contacts"
+  add_foreign_key "group_contacts", "groups"
+  add_foreign_key "likes", "contacts"
+  add_foreign_key "likes", "tags"
+  add_foreign_key "memories", "contacts"
+  add_foreign_key "memories", "stories"
+  add_foreign_key "questions", "contacts"
+  add_foreign_key "stories", "attachments"
+  add_foreign_key "stories", "users"
 end
