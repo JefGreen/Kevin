4dcee9f6c59e62f29015beab78401d69e94021fd
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-10 21:26:59 -0400

    Amazzzzing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   # resources :tags %i(new create)
   resources :groups
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
