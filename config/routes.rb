b5aa3131cc24dfc5b84f6877bacf8a69876495dc
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-10 20:50:49 -0400

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,12 @@ Rails.application.routes.draw do
   resources :contacts do
     resources :likes
     resources :questions
-
-    # resources :meetings, only: %i(new create show index)
-    # resources :experiences
-    # resources :reviews, only: %i(new create)
-
   end
 
   resources :users, only: %i(new create)
   # resources :tags %i(new create)
   resources :groups
 
-
-  # resources :meetings, only: %i(update destroy confirm)
-
-
-  get 'user', to: 'pages#profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
