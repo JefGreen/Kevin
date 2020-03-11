d849a02978a44a4e7aad1e18292abeb6adcf7b51
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-10 20:49:19 -0400

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :contacts do
+    resources :likes
+    resources :questions
+
+    # resources :meetings, only: %i(new create show index)
+    # resources :experiences
+    # resources :reviews, only: %i(new create)
+
+  end
+
+  resources :users, only: %i(new create)
+  # resources :tags %i(new create)
+  resources :groups
+
+
+  # resources :meetings, only: %i(update destroy confirm)
+
+
+  get 'user', to: 'pages#profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
