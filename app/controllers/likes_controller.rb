4796808f6a91a517cc70cff6cf15010e271c797e
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-12 19:12:39 -0400

    New save

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,21 @@
 class LikesController < ApplicationController
   def new
+    @liked = Like.new
   end
 
   def create
+    liked_before = Like.find_by(contact: 'Jeff', tag: 'Rock')
+    @liked = liked_before ? liked_before : Like.new(likes_params)
+    if @liked.save
+      redirect_to contact_path(@liked)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def likes_params
+    params.require(:like).permit(:contact, :tag, :liked)
   end
 end
