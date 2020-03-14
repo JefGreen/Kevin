5a41097c74e18d7bfeaf25e38ff420c3aa30fec1
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-03-14 15:42:46 -0400

    likes controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,15 @@
 class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
   def new
     @question = Question.new
   end
 
   def create
     @question = Question.new
-
 
     if @question.save
     else
