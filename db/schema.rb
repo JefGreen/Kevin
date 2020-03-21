75b448bb3e2872e1084a4205a5b9d31b4ae7a884
Lotfi Kaced <lotfikaced@gmail.com>
2020-03-21 15:05:57 -0400

    css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_15_84_736511) do
+ActiveRecord::Schema.define(version: 2020_15_84_814752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_15_84_736511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.datetime "date"
     t.index ["attachment_id"], name: "index_stories_on_attachment_id"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
