68e0f8b96affa633e3e065a57d8e5e44e3e070a9
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-20 22:24:34 -0400

    Added pictures to the seed. Now at 90

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ApplicationRecord
   has_one_attached :photo
   has_many :likes, dependent: :destroy
   has_many :group_contacts, dependent: :destroy
+  has_many :groups, through: :group_contacts
   has_many :questions, dependent: :destroy
   has_many :memories
+  has_namy :stories, through: :memories
 end
