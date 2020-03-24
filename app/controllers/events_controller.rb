eea9aeb4111cc4606c0e37a940a10b0f108885b0
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-03-24 19:23:31 -0400

    meeting and events working

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+  before_action :find_meeting
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.meeting_id = @meeting.id
+    if @event.save
+      redirect_to meeting_path(@meeting)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_meeting
+    @meeting = Meeting.find(params[:meeting_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:contact_id, :meeting_id)
+  end
+
+
+end
+
