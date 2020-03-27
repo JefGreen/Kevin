bc367466a3c7f81e5a7ede54b5687b279dff99b9
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-03-26 21:46:03 -0400

    adding maps

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :contacts, through: :events
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
