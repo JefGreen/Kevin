eaa0708616dc9ce43d52e8aa4097fc3e0ae492ca
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-03-27 07:32:35 -0400

    fixing map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,11 @@ import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { quiz } from "./plugins/quiz";
-
 import { initMapbox } from './plugins/init_mapbox';
 
+import { quiz } from "./plugins/quiz";
 
+initMapbox();
 quiz();
 
 
- // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
-initMapbox();
