7f6a2eadacf319bdf326e406cd377ca438b5cde9
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-28 00:25:39 -0400

    Testing out with regular action form

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -38,8 +38,8 @@ class LikesController < ApplicationController
 
   def destroy
     @liked = Like.find(params[:id])
-    question = Question.find_by(question: "Does #{@contact.first_name} #{@contact.last_name} like #{@liked.tag.name}?")
-    question.destroy
+    # question = Question.find_by(question: "Does #{@contact.first_name} #{@contact.last_name} like #{@liked.tag.name}?")
+    # question.destroy
     @liked.destroy
     redirect_to contact_path(@contact)
   end
