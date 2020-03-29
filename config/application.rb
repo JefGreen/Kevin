f93aaf1b6c367162be1cc61defe7a5718da3cd52
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-29 17:15:42 -0400

    Added routes, error controller and error views

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.exceptions_app = self.routes
 
 module Kevin
   class Application < Rails::Application
@@ -15,6 +16,7 @@ module Kevin
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
