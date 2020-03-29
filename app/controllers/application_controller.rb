094d5fdd100a260b6d15f6b37dbddc373041aae0
Jeff Trempe <trempe.jeff@gmail.com>
2020-03-29 00:19:25 -0400

    Added the title tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  module ApplicationHelper
+    def title(text)
+      content_for :title, text
+    end
+  end
 end
