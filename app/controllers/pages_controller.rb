7c362075053f80237c102ee8256c6ebae1fd77f2
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-04-01 15:47:29 -0400

    adding the score to the home (meetings)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,26 @@ class PagesController < ApplicationController
   end
 
   def welcome
-    @sum = 0
 
+    # meeting percentage
+
+    @meeting = Meeting.all.order('start_time ASC').first
+
+    @sum_meeting = 0
+    @meeting.contacts.each do |contact|
+      @questions_contacts = contact.questions
+      if @questions_contacts.count.zero?
+        @percentage1 = 0
+      else
+        @percentage1 = @questions_contacts.where(score: 100).count * 100 / @questions_contacts.count
+      end
+      @sum_meeting = @percentage1 + @sum_meeting
+    end
+    @meeting_percentage = @meeting.contacts.count.zero? ? 0 : @sum_meeting / @meeting.contacts.count
+
+    # general percentage
+
+    @sum = 0
     @questions = Question.all
       if @questions.count.zero?
         @percentage = 0
